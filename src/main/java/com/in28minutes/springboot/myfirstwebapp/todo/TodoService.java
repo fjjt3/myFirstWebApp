120f64e7e7af7a1e22c03d8ea0e69720b9565e27
fjjt3 <fjjt3@yahoo.com>
2023-10-27 12:27:05 +0200

    added todo components

---
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -0,0 +1,23 @@
+package com.in28minutes.springboot.myfirstwebapp.todo;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TodoService {
+    private static List<Todo> todos = new ArrayList<>();
+    static {
+        todos.add(new Todo(1, "Juan", "Learn AWS",
+                LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(2, "Juan", "Learn Azure",
+                LocalDate.now().plusYears(1), false));
+    }
+
+    public List<Todo> findByUsername(String username){
+        return todos;
+    }
+
+}
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title> List Todos Page </title>
+	</head>
+	<body>
+		<div>Welcome Todos!</div>
+		<div>Your Todos are : ${todos}</div>
+	</body>
+</html>
